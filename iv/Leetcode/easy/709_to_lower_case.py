a5f01dcb830661f800b130b1fa8fca38fd64b2e0
Suman Banerjee <iamsuman@gmail.com>
2021-03-20 15:08:08 -0700

    20210320

--- iv/Leetcode/easy/709_to_lower_case.py
+++ iv/Leetcode/easy/709_to_lower_case.py
@@ -9,13 +9,24 @@ class Solution:
         return "".join(res)
 
     def toLowerCase2(self, str: str) -> str:
-        s =
-        while s == "Z"
+        s = "".join([chr(i) for i in range(65, 91)])
+        t = "".join([chr(i) for i in range(97,123)])
+        res = []
+        for i in range(len(str)):
+            # ind = s.index(str[i])
+            if str[i] in s:
+                ind = s.index(str[i])
+                res.append(t[ind])
+            else:
+                res.append(str[i])
+        return "".join(res)
+
 
 
 str = "Hello"
 str = "here"
-str = "i797"
-# str = ""
+# str = "i797"
+str = ""
 s = Solution()
 print(s.toLowerCase(str))
+print(s.toLowerCase2(str))
